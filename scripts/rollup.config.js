dd6f388697c8451f50e51eb326c81b36e10736c6
二货机器人 <smith3816@gmail.com>
2024-06-02 20:23:59 +0800

    bump

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,13 +1,13 @@
-const generate = require('videojs-generate-rollup-config');
-const replace = require('@rollup/plugin-replace');
-const dataFiles = require('rollup-plugin-data-files');
+const generate = require("videojs-generate-rollup-config");
+const replace = require("@rollup/plugin-replace");
+const dataFiles = require("rollup-plugin-data-files");
 
 // see https://github.com/videojs/videojs-generate-rollup-config
 // for options
 const options = {
-  input: 'src/index.js',
+  input: "src/index.js",
   externals(defaults) {
-    defaults.module.push('@videojs/vhs-utils');
+    defaults.module.push("@videojs/vhs-utils");
 
     return defaults;
   },
@@ -18,27 +18,38 @@ const options = {
       // single quote replace
       "require('@videojs/vhs-utils/es": "require('@videojs/vhs-utils/cjs",
       // double quote replace
-      'require("@videojs/vhs-utils/es': 'require("@videojs/vhs-utils/cjs'
+      'require("@videojs/vhs-utils/es': 'require("@videojs/vhs-utils/cjs',
     });
 
     defaults.dataFiles = dataFiles({
-      expecteds: {include: 'test/fixtures/integration/*.js', transform: 'js', extensions: false},
-      manifests: {include: 'test/fixtures/integration/*.m3u8', transform: 'string', extensions: false}
+      expecteds: {
+        include: "test/fixtures/integration/*.js",
+        transform: "js",
+        extensions: false,
+      },
+      manifests: {
+        include: "test/fixtures/integration/*.m3u8",
+        transform: "string",
+        extensions: false,
+      },
     });
 
     return defaults;
   },
   plugins(defaults) {
-    defaults.module.unshift('replace');
-    defaults.test.unshift('dataFiles');
+    defaults.module.unshift("replace");
+    defaults.test.unshift("dataFiles");
 
     return defaults;
-  }
+  },
+
+  // Build in dist as `dist/m3u8-parser.cjs.js` & `dist/m3u8-parser.es.js`
+  distName: "m3u8-parser",
 };
 const config = generate(options);
 
 if (config.builds.test) {
-  config.builds.test.output[0].format = 'umd';
+  config.builds.test.output[0].format = "umd";
 }
 
 // Add additonal builds/customization here!
